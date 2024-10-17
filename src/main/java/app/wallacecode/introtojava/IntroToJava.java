053000a9da5fad78038842f17af1a67cdb80dbda
walla <walla@qtech>
2024-10-17 16:24:36 -0500

    First Commit

---
+++ src/main/java/app/wallacecode/introtojava/IntroToJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package app.wallacecode.introtojava;
+
+/**
+ *
+ * @author walla
+ */
+public class IntroToJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
